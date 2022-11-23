54444399c937d80e050528ebb8860d2197693a19
Daniel <danielislas22@outlook.es>
2022-11-22 21:24:47 -0600

    se agrego una funcion foreach para sumar los elementos del menu

--- src/js/app.js
+++ src/js/app.js
@@ -40,6 +40,66 @@ element.appendChild(p);
 nombres = ['Kg','Taco','Dorado','1LtCSM','1/2CSM','Boing','Cafe','Refresco'];
 precios = [440,30,24,50,25,20,20,25];
 
+const menuData =[{
+    name: 'Kg',
+    price: 440,
+},
+{
+    name: 'Taco',
+    price: 30, 
+},
+{
+    name: 'Dorado',
+    price: 24,
+},
+{
+    name: '1LtCSM',
+    price: 50,
+},
+{
+    name: '1/2CSM',
+    price:25,
+},
+{
+    name: 'Boing',
+    price: 20,
+},
+{
+    name: 'Cafe',
+    price:20,
+},
+{
+    name: 'Refresco',
+    price:25,
+},
+];
+
+generateGrid(menuData, document.querySelector('.icons-container'));
+
+function generateGrid(menuData, parent) {
+    let total =0;
+
+    menuData.forEach((menuItem) => {
+        const btn = document.createElement('button');
+        const h3 = document.createElement('h3');
+
+        btn.appendChild(h3);
+        parent.appendChild(btn);
+
+        h3.textContent = menuItem.name;
+
+        btn.addEventListener('click',() => {
+            const priceEl = document.getElementById('price');
+            const totalEl = document.getElementById('total');
+
+            total += menuItem.price;
+
+            priceEl.textContent = menuItem.price;
+            totalEl.textContent = total;
+        });
+    });
+}
+
 Total = [];
 
 
@@ -52,6 +112,7 @@ for(i=0;i<=7;i++){
     
     div.classList.add('cuadro');
     div.appendChild(h3);
+    div.value = `valor${i}`;
     h3.textContent = `${nombres[i]}`;
     h3.classList.add('icon-text');
     element.appendChild(div);
