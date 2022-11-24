00f0384965367985a5dc1a009e8c06fdffa3476d
Daniel <danielislas22@outlook.es>
2022-11-23 18:53:45 -0600

    se mejoro el display de manera que sea visible la cantidad que se agrega de cada producto con ayuda de un if else y un agregado y generador de id

--- build/js/app.js
+++ build/js/app.js
@@ -43,41 +43,51 @@ precios = [440,30,24,50,25,20,20,25];
 const menuData =[{
     name: 'Kg',
     price: 440,
+    cantidad:0,
 },
 {
     name: 'Taco',
-    price: 30, 
+    price: 30,
+    cantidad:0, 
 },
 {
     name: 'Dorado',
     price: 24,
+    cantidad:0, 
 },
 {
     name: '1LtCSM',
     price: 50,
+    cantidad:0, 
 },
 {
     name: '1/2CSM',
     price:25,
+    cantidad:0, 
 },
 {
     name: 'Boing',
     price: 20,
+    cantidad:0, 
 },
 {
     name: 'Cafe',
     price:20,
+    cantidad:0, 
 },
 {
     name: 'Refresco',
     price:25,
+    cantidad:0, 
 },
 ];
 
+const compras = document.querySelector('.compras');
 generateGrid(menuData, document.querySelector('.icons-container'));
 
 function generateGrid(menuData, parent) {
-    let total =0;
+    let total = 0;
+    let cantidad = 0;
 
     menuData.forEach((menuItem) => {
         const btn = document.createElement('button');
@@ -89,13 +99,64 @@ function generateGrid(menuData, parent) {
         h3.textContent = menuItem.name;
 
         btn.addEventListener('click',() => {
+            let counter = 1;
+            cantidad += counter;
+            menuItem.cantidad += counter
+            
+            
+            //console.log(`la cantidad en el closure es de ${menuItem.cantidad}`);
+
+            if(menuItem.cantidad===1){
+                const div = document.createElement('div');
+                div.classList.add('display-compras');
+                const item = document.createElement('p');
+                const cantidadEl = document.createElement('p');
+                const precio = document.createElement('p');
+
+
+                compras.appendChild(div);
+                div.appendChild(item);
+                div.appendChild(cantidadEl);
+                div.appendChild(precio);
+
+
+               // console.log('hay solo un producto')
+                cantidadEl.setAttribute('id',`cant${menuItem.name}`)
+                precio.setAttribute('id',`precio${menuItem.name}`)
+                precio.textContent = menuItem.price;
+                item.textContent = menuItem.name;
+                cantidadEl.textContent = menuItem.cantidad;
+                
+            }else{
+                console.log('hay mas de un producto')
+                const newdivEl = document.getElementById(`cant${menuItem.name}`);
+                const newdivc = document.getElementById(`precio${menuItem.name}`)
+                newdivEl.textContent = `${menuItem.cantidad} * ${menuItem.price}`;
+                newdivc.textContent = `$${(menuItem.price)*(menuItem.cantidad)}`;
+                console.log(`la cantidad es ${cantidad} y en el closure hay ${menuItem.cantidad} de ${menuItem.name} el total es ${menuItem.price*menuItem.cantidad}`)
+                
+                
+            }
+           
+            
+
+            
+            
+
+            
+            const totalesEl = document.getElementById('totales');
             const priceEl = document.getElementById('price');
             const totalEl = document.getElementById('total');
-
             total += menuItem.price;
 
             priceEl.textContent = menuItem.price;
             totalEl.textContent = total;
+
+
+
+            
+            
+            totalesEl.textContent = total;
         });
     });
 }
@@ -118,5 +179,7 @@ for(i=0;i<=7;i++){
     element.appendChild(div);
     
 };
+
+
 
 
